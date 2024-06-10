6b5d1bdda4d06c9a9972ab5b3c3c91120f1e0706
rustem <>
2024-06-10 13:27:17 +0300

    optimized iterations

--- src/gymnasium/dataset.py
+++ src/gymnasium/dataset.py
@@ -22,7 +22,7 @@ orderbook = conob.read(
 orderbook.columns = ['time', 'bprice', 'bvol', 'aprice', 'avol']
 orderbook['time'] = pd.to_datetime(orderbook['time'], format='mixed')\
     .dt.tz_localize(None)
-print(orderbook)
+orderbook['date'] = orderbook['time'].dt.date
 
 trades = contr.read(
     """
@@ -35,52 +35,57 @@ trades = contr.read(
 trades.columns = ['time', 'direction', 'price', 'vol']
 trades['time'] = pd.to_datetime(trades['time'], format='mixed')\
     .dt.tz_localize(None)
-print(trades)
+trades['date'] = trades['time'].dt.date
 
-ts_start = orderbook['time'].iloc[0]
-ts_end = ts_start + timedelta(seconds=1)
-bpoint = orderbook['time'].iloc[-1]
+dates = orderbook['date'].unique()
+orderbook = orderbook.groupby('date')
+trades = trades.groupby('date')
 rows = []
-tr = pd.DataFrame({
-    'price': [0],
-    'vol': [0],
-    'direction': [0]
-})
-ob = pd.DataFrame({
-    'bprice': [0],
-    'bvol': [0],
-    'aprice': [0],
-    'avol': [0]
-})
-while ts_end <= bpoint:
-    tmp_trades = trades[
-        (trades['time'] >= ts_start) & (trades['time'] < ts_end)]
-    if not tmp_trades.empty:
-        tr = tmp_trades
-        trades = trades.drop(tmp_trades.index)
-    tmp_orderbook = orderbook[
-        (orderbook['time'] >= ts_start) & (orderbook['time'] < ts_end)]
-    if not tmp_orderbook.empty:
-        ob = tmp_orderbook
-        orderbook = orderbook.drop(tmp_orderbook.index)
-    ts_start = ts_end
-    ts_end += timedelta(seconds=1)
-    row = {
-        'time': ts_end,
-        'open': tr['price'].iloc[0],
-        'high': tr['price'].max(),
-        'low': tr['price'].min(),
-        'close': tr['price'].iloc[-1],
-        'volume': tr['vol'].sum(),
-        'feature_direction_1': sum([x == 1 for x in tr['direction']]),
-        'feature_direction_2': sum([x == 2 for x in tr['direction']]),
-        'feature_mean_bprice': ob['bprice'].mean(),
-        'feature_mean_bvol': ob['bvol'].mean(),
-        'feature_mean_aprice': ob['aprice'].mean(),
-        'feature_mean_avol': ob['avol'].mean()
-    }
-    rows.append(row)
-
-    print(f'Lines to the end: {orderbook.shape[0]}')
-pd.DataFrame(rows).to_csv(
+for date in dates:
+    filtered_orderbook = orderbook.get_group(date)
+    filtered_trades = trades.get_group(date)
+    ts_start = filtered_orderbook['time'].iloc[0]
+    ts_end = ts_start + timedelta(seconds=1)
+    bpoint = filtered_orderbook['time'].iloc[-1]
+    tr = pd.DataFrame({
+        'price': [0],
+        'vol': [0],
+        'direction': [0]
+    })
+    ob = pd.DataFrame({
+        'bprice': [0],
+        'bvol': [0],
+        'aprice': [0],
+        'avol': [0]
+    })
+    while ts_end <= bpoint:
+        tmp_trades = filtered_trades[
+            (filtered_trades['time'] >= ts_start) & (filtered_trades['time'] < ts_end)]  # noqa: E501
+        if not tmp_trades.empty:
+            tr = tmp_trades
+            filtered_trades = filtered_trades.drop(tmp_trades.index)
+        tmp_orderbook = filtered_orderbook[
+            (filtered_orderbook['time'] >= ts_start) & (filtered_orderbook['time'] < ts_end)]  # noqa: E501
+        if not tmp_orderbook.empty:
+            ob = tmp_orderbook
+            filtered_orderbook = filtered_orderbook.drop(tmp_orderbook.index)
+        ts_start = ts_end
+        ts_end += timedelta(seconds=1)
+        row = {
+            'time': ts_end,
+            'open': tr['price'].iloc[0],
+            'high': tr['price'].max(),
+            'low': tr['price'].min(),
+            'close': tr['price'].iloc[-1],
+            'volume': tr['vol'].sum(),
+            'feature_direction_1': sum([x == 1 for x in tr['direction']]),
+            'feature_direction_2': sum([x == 2 for x in tr['direction']]),
+            'feature_mean_bprice': ob['bprice'].mean(),
+            'feature_mean_bvol': ob['bvol'].mean(),
+            'feature_mean_aprice': ob['aprice'].mean(),
+            'feature_mean_avol': ob['avol'].mean()
+        }
+        rows.append(row)
+        print(f'Lines to the end: {filtered_orderbook.shape[0]}')
+pd.DataFrame(rows).drop_duplicates().reset_index(drop=True).to_csv(
     os.path.join(os.getcwd(), 'data', 'interim', 'dataset.csv'))
