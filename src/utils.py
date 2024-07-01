e40258aa3cc3b3e436b2d9f4b6916a7ea0b9f89a
rustem <>
2024-07-01 18:53:19 +0300

    added funs

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,13 @@
 import pickle
-from typing import Dict
-from tinkoff.invest import Order
+import pandas as pd
+from typing import Dict, List, Union
+from tinkoff.invest import Order, HistoricCandle, Quotation, MoneyValue
 from tinkoff.invest.utils import quotation_to_decimal
+
+
+def price_to_float(price: Union[Quotation, MoneyValue]) -> float:
+    fractional = price.nano / 10e8
+    return price.units + fractional
 
 
 def OrderToDict(order: Order) -> Dict[float, int]:
@@ -19,3 +25,19 @@ def load(path):
     with open(path, 'rb') as obj_file:
         obj = pickle.load(obj_file)
     return obj
+
+
+def historicCandles_to_df(candles: List[HistoricCandle]) -> pd.DataFrame:
+    rows = []
+    for candle in candles:
+        rows.append(
+            {
+                'open': float(quotation_to_decimal(candle.open)),
+                'high': float(quotation_to_decimal(candle.high)),
+                'low': float(quotation_to_decimal(candle.low)),
+                'close': float(quotation_to_decimal(candle.close)),
+                'volume': candle.volume,
+                'time': candle.time
+            }
+        )
+    return pd.DataFrame(rows)
