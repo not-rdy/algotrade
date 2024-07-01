0b1424594fd6bb9becd50bde27a40264009b0e6d
rustem <>
2024-07-01 18:52:53 +0300

    added candles loader

---
+++ src/async_all_candles.py
@@ -0,0 +1,37 @@
+import os
+import asyncio
+import argparse
+import pandas as pd
+from datetime import timedelta
+from utils import historicCandles_to_df
+from tinkoff.invest import AsyncClient, CandleInterval
+from tinkoff.invest.utils import now
+
+candles = []
+
+
+async def main():
+    async with AsyncClient(os.getenv('TOKEN')) as client:
+        async for candle in client.get_all_candles(
+            instrument_id='BBG004730N88',
+            from_=now() - timedelta(days=DAYS),
+            interval=INTERVAL,
+        ):
+            candles.append(candle)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-days', type=int, help='The number of the days in the past')
+    args = parser.parse_args()
+
+    DAYS = args.days
+    INTERVAL = CandleInterval.CANDLE_INTERVAL_1_MIN
+    NAME = 'candles_SBER_min.csv'
+    PATH_PROJ = os.getcwd()
+
+    asyncio.run(main())
+    pd.DataFrame(historicCandles_to_df(candles)).to_csv(
+        os.path.join(PATH_PROJ, 'data', 'raw', NAME))
